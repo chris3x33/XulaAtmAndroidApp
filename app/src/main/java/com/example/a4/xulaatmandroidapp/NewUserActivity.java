7860b45c380455a78568fd49512d4f1e5523e009
chris3x33 <chris3x33g@gmail.com>
2017-10-03 17:15:38 -0500

    Added GUI components to new_user_layout and NewUserActivity

--- app/src/main/java/com/example/a4/xulaatmandroidapp/NewUserActivity.java
+++ app/src/main/java/com/example/a4/xulaatmandroidapp/NewUserActivity.java
@@ -5,11 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class NewUserActivity extends AppCompatActivity {
 
     private TextView newUserMsgLbl;
+    private EditText userNameTxt;
+    private EditText passwordTxt;
+
     private Context context;
 
     @Override
@@ -20,9 +24,17 @@ public class NewUserActivity extends AppCompatActivity {
         context = this;
 
         newUserMsgLbl = (TextView) findViewById(R.id.newUserMsgLbl);
+        userNameTxt = (EditText) findViewById(R.id.userNameTxt);
+        passwordTxt = (EditText) findViewById(R.id.passwordTxt);
 
         newUserMsgLbl.setText("Hello World!");
+
     }
 
 
+    public void register(View view) {
+    }
+
+    public void back(View view) {
+    }
 }
