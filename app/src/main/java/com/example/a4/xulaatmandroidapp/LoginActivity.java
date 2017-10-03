06d037483885e240aec3c40c9db9dc733268e0da
chris3x33 <chris3x33g@gmail.com>
2017-10-03 16:31:33 -0500

    Added GUI components to LoginActivity and login_layout

--- app/src/main/java/com/example/a4/xulaatmandroidapp/LoginActivity.java
+++ app/src/main/java/com/example/a4/xulaatmandroidapp/LoginActivity.java
@@ -3,11 +3,14 @@ package com.example.a4.xulaatmandroidapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView loginMsgLbl;
+    private EditText userNameTxt;
+    private EditText passwordTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +18,17 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login_layout);
 
         loginMsgLbl = (TextView) findViewById(R.id.loginMsgLbl);
+        userNameTxt = (EditText) findViewById(R.id.userNameTxt);
+        passwordTxt = (EditText) findViewById(R.id.passwordTxt);
 
         loginMsgLbl.setText("Login!");
 
     }
 
 
+    public void login(View view) {
+    }
+
+    public void back(View view) {
+    }
 }
